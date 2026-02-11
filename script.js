9c8bcdf71ffa80a17043d32e488b42ece6dad719
vlad615 <ambrosov413@gmail.com>
2026-02-11 15:11:22 +0300

    less 13

--- script.js
+++ script.js
@@ -12,13 +12,14 @@ const model = {
     },
 
     render(){
-        console.clear();
-        console.log(this.courseData);
+        document.body.innerHTML = ""
+        let view = CourseView(this.courseData)
+        document.body.append(view)
     }, 
 
     createLesson(LessonTitle){
         let newLesson = {
-            id: 6,
+            id: new Date().getTime(),
             title: LessonTitle, 
             isDone: false
         }
@@ -41,13 +42,97 @@ const model = {
         this.render();
     },
 
-    updateTitle(Title){
-        const newState = {title: Title, lessons: [...this.courseData.lessons]}
+    updatelessonTitle(lessonId, Title){
+        const newState = this.courseData.lessons.map(x => x.id == lessonId ? {...x, title: Title} : x);
+        this.courseData.lessons = newState;
+        this.render();
     },
+
+    updateTitle(Title){
+        const newState = {...this.courseData, title: Title}
+        this.courseData = newState;
+        this.render();
+    },
+    
 }
 
 function init(){
-    model.render();
+    model.render()
 }
 
 init();
+
+function CourseView(data){
+    const section = document.createElement("section")
+
+    const title = createTitle(data.title)
+
+    const message = document.createElement("div")
+    message.classList.add("green")
+    const form = createForm(message)
+
+    const list = createList(data.lessons)
+
+
+
+    section.append(
+        title, form, list, message
+    )
+
+    return section
+}
+
+
+function createTitle(Title){
+    const title = document.createElement("h1")
+    title.setAttribute("id", "course-title")
+    title.classList.add("title")
+    title.textContent = Title
+
+    return title
+}
+
+function createForm(message) {
+    const form = document.createElement("form")
+    form.classList.add("list-form")
+    form.setAttribute('action', 'post')
+    form.innerHTML = `
+        <input type="text" name="lesson" placeholder="введите название урока" required />
+        <button type="submit">Добавить</button>
+    `
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault()
+        const value = form.lesson.value
+
+        message.textContent = "Adding ..."
+        setTimeout(()=>{
+            model.createLesson(value)
+            message.textContent = ''
+        }, 1000)
+            
+    })
+
+    return form
+}
+
+function createList(lessons){
+    const ul = document.createElement("ul")
+    ul.classList.add("list")
+
+    lessons.forEach(x => {
+        const li = document.createElement("li")
+        li.classList.add("item")
+        li.textContent = x.title
+        if (x.isDone) {li.classList.add("done")}
+        li.addEventListener("dblclick", () => model.updateLesson(x.id))
+        const but_del = document.createElement("button")
+        but_del.classList.add("delete")
+        but_del.textContent = "X"
+
+        but_del.addEventListener("click", () => model.deleteLesson(x.id))
+        li.append(but_del)
+        ul.append(li)
+    })
+
+    return ul
+}
